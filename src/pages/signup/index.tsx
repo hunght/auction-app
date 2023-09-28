d357fa0803963092a25614c9d17eedd4e152babe
hunghoang <hth321@gmail.com>
2023-09-28 22:51:49 +0700

    update

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -23,8 +23,8 @@ const SignupPage: React.FC = () => {
       .catch(() => {
         // Handle errors
         toast({
-          title: "Scheduled: Catch up",
-          description: "Friday, February 10, 2023 at 5:57 PM",
+          title: "Error",
+          description: "Something went wrong. Please try again later.",
           variant: "destructive",
         });
       });
