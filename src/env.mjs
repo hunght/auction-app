ee24965c9979941b89ee1b4111439f8d7752d25e
hunghoang <hth321@gmail.com>
2023-10-06 10:11:04 +0700

    fix

--- src/env.mjs
+++ src/env.mjs
@@ -1,7 +1,7 @@
 import { createEnv } from '@t3-oss/env-nextjs';
 import { z } from 'zod';
 import { config } from 'dotenv';
-config();
+config({ path: '.env' });
 export const env = createEnv({
   /**
    * Specify your server-side environment variables schema here. This way you can ensure the app
